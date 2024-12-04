8465e11a420e3cba55247f1c15bf5a33a95824b8
Max Parker <maxrparker@gmail.com>
2024-12-04 15:47:39 -0800

    Day 4 with part1 redo. Jank.

--- day4/puzzle2.ts
+++ day4/puzzle2.ts
@@ -1,1 +1,31 @@
-// now just use the diagonals, pin down the index of each mas, and check if the center matches the index in the second diagonal array
+import * as fs from "fs";
+
+const input = fs.readFileSync("./input.txt", "utf8");
+
+let start = new Date().getTime();
+
+let count = 0;
+
+const inputArray = input.split("\n").map((row) => [...row]);
+
+for (let i = 1; i < inputArray.length - 1; i++) {
+  for (let j = 0; j < inputArray[i].length - 1; j++) {
+    if (inputArray[i][j] == "A") {
+      if (
+        ((inputArray[i - 1][j - 1] == "M" && inputArray[i + 1][j + 1] == "S") ||
+          (inputArray[i - 1][j - 1] == "S" &&
+            inputArray[i + 1][j + 1] == "M")) &&
+        ((inputArray[i - 1][j + 1] == "M" && inputArray[i + 1][j - 1] == "S") ||
+          (inputArray[i - 1][j + 1] == "S" && inputArray[i + 1][j - 1] == "M"))
+      ) {
+        count++;
+      }
+    }
+  }
+}
+
+let end = new Date().getTime();
+
+console.log("Execution time: " + (end - start) + "ms"); //1ms
+
+console.log(count);
